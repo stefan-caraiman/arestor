c7d67e799436bec53b04862c607b5a5b134c83e7
Alexandru Coman <acoman@cloudbasesolutions.com>
2016-12-13 15:24:02 +0200

    [WIP] Add support for resource management

--- arestor/common/exception.py
+++ arestor/common/exception.py
@@ -49,6 +49,13 @@ class CliError(ArestorException):
     template = "Something went wrong during the procesing of command line."
 
 
+class ClientError(ArestorException):
+
+    """Something went wrong during communication with the API."""
+
+    template = "Something went wrong during communication with the API."
+
+
 class Invalid(ArestorException):
 
     """The received object is not valid."""
